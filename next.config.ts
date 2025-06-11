037ac8228e4eaf642b19b019366e5b8d3a516863
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2025-06-11 18:49:46 +0100

    changed next.config to work with github actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  basePath: "/personal-blog",
+  output: "export",
   reactStrictMode: true,
 };
 
