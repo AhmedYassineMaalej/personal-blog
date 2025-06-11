d7e560d28cb3c884a2a023e2117b544b0d71e0ee
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2025-06-11 19:41:56 +0100

    reset all config to work with vercel

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/personal-blog",
-  output: "export",
   reactStrictMode: true,
 };
 
 export default nextConfig;
+
